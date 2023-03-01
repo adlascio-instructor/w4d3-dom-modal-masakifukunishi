4ecfacd81141decf8de211dcf524f67f74b4b542
MasakiFukunishi <fmsknn@gmail.com>
2023-03-01 12:28:58 -0800

    Add addEventListener

--- modal.js
+++ modal.js
@@ -1,9 +1,19 @@
 console.log("here");
 
 // select button element
+const openButton = document.getElementById("openModal");
 
 // add event listener on click to button
 // remove class invisible from div modal
+openButton.addEventListener("click", () => {
+  const modal = document.getElementById("modal");
+  modal.classList.remove("invisible");
+});
 
 // add event listener on key press Escape
 // add class invisible to div modal
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    modal.classList.add("invisible");
+  }
+});
